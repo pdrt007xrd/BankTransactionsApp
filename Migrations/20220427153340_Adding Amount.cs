8689021583e3d01f3f8943acaa11b4b7532f8b0e
MrManhatan <MrManhatan@DESKTOP-JS4D2NV>
2022-04-27 14:30:51 -0400

    Agregar archivos de proyecto.

---
+++ Migrations/20220427153340_Adding Amount.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankTransactions.Migrations
+{
+    public partial class AddingAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Transactions");
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Required(ErrorMessage ="Field Required!")]
+        [Column(TypeName ="nvarchar(12)")] //to specify type of data of column in BD
+        [Display(Name="Account Number")]
+        [MaxLength(12, ErrorMessage = "Maximun 12 Characters Only.")]
+        public string AccountNumber { get; set; }
+        [Required(ErrorMessage = "Field Required!")]
+        [Display(Name="Beneficiary Name")]
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficiaryName { get; set; }
+        [Required(ErrorMessage = "Field Required!")]
+        [Display(Name ="Bank Name")]
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
+        [Required(ErrorMessage = "Field Required!")]
+        [Display(Name ="Swift Code")]
+        [Column(TypeName = "nvarchar(11)")]
+        [MaxLength(11, ErrorMessage ="Maximun 11 Characters Only.")]
+        public string SWIFTCODE { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
